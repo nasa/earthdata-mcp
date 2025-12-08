8ba73f20b28517b04e36e1813f844cccd3414d5c
Deep Mistry <deepmist2@gmail.com>
2025-12-08 12:49:22 -0500

    CMRNLP-39: Adds bamboo-bin.sh file

---
+++ terraform/scripts/bamboo-bins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Retrieve a copy of terraform and aws.
+# Make available on path.
+
+curr_dir=$PWD
+tf_version=$(cat .terraform_version)
+mkdir -p exec_bin
+cd exec_bin
+
+echo "pulling a terraform script."
+if ! curl -o terraform_${tf_version}_linux_amd64.zip https://releases.hashicorp.com/terraform/${tf_version}/terraform_${tf_version}_linux_amd64.zip ; then
+  echo "ERROR: coudn't download terraform script" >&2
+  exit 1
+else
+  unzip -u terraform_${tf_version}_linux_amd64.zip
+  chmod a+x terraform
+  rm terraform_${tf_version}_linux_amd64.zip
+fi
+
+export PATH=$PWD:$PATH
+cd $curr_dir
