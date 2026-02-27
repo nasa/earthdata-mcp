b6d711b0339d8659946fd26c21ab13a888f62067
clement li <clement.h.li@nasa.gov>
2026-02-27 14:45:09 -0500

    CMRNLP-75 missing init.py in models/rag_eval

---
+++ models/rag_eval/__init__.py
@@ -0,0 +1,13 @@
+"""RAG evaluation models."""
+
+from models.rag_eval.evaluation import (
+    DatasetRelevanceInput,
+    DatasetRelevancePrompt,
+    DatasetRelevanceScore,
+)
+
+__all__ = [
+    "DatasetRelevanceInput",
+    "DatasetRelevancePrompt",
+    "DatasetRelevanceScore",
+]
