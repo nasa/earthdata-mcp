28297c0d1c1e0021d1092b2a03619fbdaf63a733
Matthew Crouch <mcrouch@innovim.com>
2026-09-09 12:26:02 -0400

    CMRNLP-152: Updates ALB listener rule

--- terraform/modules/application/mcp-server.tf
+++ terraform/modules/application/mcp-server.tf
@@ -187,7 +187,7 @@ resource "aws_lb_listener_rule" "mcp" {
 
   condition {
     path_pattern {
-      values = ["/mcp", "/mcp/*"]
+      values = ["/mcp", "/mcp/*", "/.well-known/*/mcp", "/.well-known/*/mcp/*"]
     }
   }
 
